0052f6a243cae0762a310166a63424262959a79a
Matthew Butterick <mb@mbtype.com>
2015-01-06 11:09:31 -0800

    require v6

--- info.rkt
+++ info.rkt
@@ -1,5 +1,5 @@
 #lang info
 (define collection "css-tools")
 (define scribblings '(("scribblings/css-tools.scrbl" ())))
-(define deps '("base" "sugar"))
+(define deps '(("base" #:version "6.0") "sugar"))
 (define build-deps '("scribble-lib"))
